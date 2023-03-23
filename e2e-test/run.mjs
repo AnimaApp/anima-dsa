8fd32c9c26a648f173e4f7498edc38efb1bff480
Or Arbel <orarbel@gmail.com>
2023-03-23 15:49:28 +0200

    add e2e test

--- e2e-test/run.mjs
+++ e2e-test/run.mjs
@@ -4,15 +4,33 @@ const token = process.env.TEST_TOKEN; // personal team created for it
 if (!token) throw new Error('Anima team token for test not found');
 const storyHashReg = new RegExp(/hash => (.*)/);
 
-// --- Setup ---
+// ----------------- Test Simple Sync -----------------
+
 console.log('Setup, running sync');
-const output = execSync(
+let output = execSync(
   `node ./dist/cli.js sync -d "s3://anima-uploads/e2e-tests/storybook-sample" -t ${token} --debug`,
 );
 
-const result = storyHashReg.exec(output.toString());
+let result = storyHashReg.exec(output.toString());
 if (!result) throw new Error('storybook hash not found');
-const [, storybookHash] = result;
+let [, storybookHash] = result;
+
+console.log('Run the tests');
+// --- Tests ---
+// Run tests Check if stories are well created and synced and finished processing
+execSync(`TOKEN=${token} HASH=${storybookHash} yarn jest ./`, {
+  stdio: 'inherit',
+});
+
+// ----------------- Test Sync with base path -----------------
+console.log('Setup, running sync with base path');
+output = execSync(
+  `node ./dist/cli.js sync -d "s3://anima-uploads/e2e-tests/storybook-sample-base-path" -t ${token} --debug -b /styleguide`,
+);
+
+result = storyHashReg.exec(output.toString());
+if (!result) throw new Error('storybook hash not found');
+[, storybookHash] = result;
 
 console.log('Run the tests');
 // --- Tests ---
