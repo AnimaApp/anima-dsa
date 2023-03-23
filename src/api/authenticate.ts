d5fb8faee9c61ba063e08fa1cc06eb9878770feb
Or Arbel <orarbel@gmail.com>
2023-03-23 11:21:43 +0200

    cleanup

--- src/api/authenticate.ts
+++ src/api/authenticate.ts
@@ -4,23 +4,26 @@ import { STORYBOOK_SERVICE_BASE_URL } from '../constants';
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export const authenticate = async (storybookToken: string) => {
   const errorRes = { success: false, data: {} };
-  console.log(storybookToken)
-  if (!storybookToken) return errorRes;
-  const res = await nf(`${STORYBOOK_SERVICE_BASE_URL}/validate_token`, {
-    method: 'GET',
-    headers: {
-      Authorization: 'Bearer ' + storybookToken,
-    },
-  });
-  console.log(res.status, res.statusText)
-  if (res.status === 200) {
-    const data = await res.json();
-    return { success: true, data };
+  try {
+    if (!storybookToken) return errorRes;
+    const res = await nf(`${STORYBOOK_SERVICE_BASE_URL}/validate_token`, {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + storybookToken,
+      },
+    });
+    if (res.status === 200) {
+      const data = await res.json();
+      return { success: true, data };
+    }
+    if (res.status > 299) {
+      const json = await res.json();
+      const message = json?.message || 'Missing Storybook token';
+      return { success: false, message };
+    }
+    return errorRes;
+  } catch (error) {
+    console.log(error);
+    return errorRes;
   }
-  if (res.status > 299) {
-    const json = await res.json();
-    const message = json?.message || 'Missing Storybook token';
-    return { success: false, message };
-  }
-  return errorRes;
 };
