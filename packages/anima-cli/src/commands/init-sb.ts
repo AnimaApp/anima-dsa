35301af4bc4d7de4ae71856acb3bf27430366ce3
BenAAndrew <benandrew89@gmail.com>
2023-06-08 15:12:11 +0200

    add init-sb command

---
+++ packages/anima-cli/src/commands/init-sb.ts
@@ -0,0 +1,57 @@
+import { Arguments, CommandBuilder } from 'yargs';
+import { loader } from '../helpers/loader';
+import * as Sentry from '@sentry/node';
+import fs from 'fs';
+import { log } from '../helpers';
+import { generateStorybookConfig, getJSFiles } from '../helpers/storybook';
+import { getToken } from '../helpers/token';
+import { authenticate } from '../api';
+
+export const command = 'init-sb';
+export const desc = 'Initialise storybook on your project';
+
+export const builder: CommandBuilder = (yargs) =>
+  yargs
+    .options({
+      token: { type: 'string', alias: 't' },
+      componentsDir: { type: 'string', alias: 'c' },
+      debug: { type: 'boolean' },
+    })
+    .example([['$0 init-sb -c <components-dir>']]);
+
+export const handler = async (_argv: Arguments): Promise<void> => {
+  const transaction = Sentry.startTransaction({
+    op: 'init-sb',
+    name: 'initialise storybook',
+  });
+  try {
+    Sentry.getCurrentHub().configureScope((scope) =>
+      scope.setSpan(transaction),
+    );
+    const token = getToken(_argv);
+    await authenticate(token);
+    loader.newStage('Fetching components');
+    const files = getJSFiles(_argv.componentsDir as string);
+    const componentsWithoutStorybook = files.filter(f => !f.includes(".test.") && !files.includes(`${f.split(".")[0]}.stories.js`));
+    loader.newStage(`Creating ${componentsWithoutStorybook.length} component configurations`);
+    for(const componentFile of componentsWithoutStorybook){
+      console.log(`Creating ${componentFile}...`);
+      const componentContent = fs.readFileSync(componentFile, 'utf8');
+      const response = await generateStorybookConfig(componentContent, token).catch((e) => {throw e});
+      if(response){
+        fs.writeFileSync(componentFile, response.code);
+        fs.writeFileSync(`${componentFile.split(".")[0]}.stories.js`, response.story);
+      } else {
+        console.log(`Skipped ${componentFile}. Couldn't generate story config`);
+      }
+    }
+    loader.stop();
+    log.green('  - Done');
+    transaction.status = 'ok';
+    transaction.finish();
+  } catch (e) {
+    transaction.status = 'error';
+    transaction.finish();
+    throw e;
+  }
+};
---
+++ packages/anima-cli/src/helpers/storybook.ts
@@ -0,0 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+import { API_URL } from '../constants/api';
+
+const JS_EXTENSIONS = ["js", "jsx", "ts", "tsx", "vue"];
+
+export const getJSFiles = (folder: string) => {
+    const JSFiles: string[] = [];
+
+    for (const item of fs.readdirSync(folder)) {
+        const fullPath = path.join(folder, item);
+        if (fs.lstatSync(fullPath).isDirectory()) {
+            JSFiles.push(...getJSFiles(fullPath));
+        } else {
+            const extension = item.split(".").pop()
+            if (extension && JS_EXTENSIONS.includes(extension) && item.charAt(0) === item.charAt(0).toUpperCase()) {
+                JSFiles.push(fullPath);
+            }
+        }
+    }
+
+    return JSFiles;
+};
+
+export const generateStorybookConfig = async (file: string, token: string) => {
+    const res = await fetch(`${API_URL}/generate_storybook/`, {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json',
+            Authorization: 'Bearer ' + token,
+        },
+        body: JSON.stringify({ code: file }),
+    });
+
+    if (res.status === 200) {
+        const data = await res.json();
+        return data;
+    }
+
+    return null;
+};
