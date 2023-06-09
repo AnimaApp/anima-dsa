3b5ccb32e275b7c2e5ba34abda85eb165b887644
BenAAndrew <benandrew89@gmail.com>
2023-06-09 14:18:00 +0200

    add storybook init command

--- packages/anima-cli/src/commands/init-sb.ts
+++ packages/anima-cli/src/commands/init-sb.ts
@@ -2,10 +2,12 @@ import { Arguments, CommandBuilder } from 'yargs';
 import { loader } from '../helpers/loader';
 import * as Sentry from '@sentry/node';
 import fs from 'fs';
+import path from 'path';
 import { log } from '../helpers';
 import { generateStorybookConfig, getJSFiles } from '../helpers/storybook';
 import { getToken } from '../helpers/token';
 import { authenticate } from '../api';
+import { execSync } from 'child_process';
 
 export const command = 'init-sb';
 export const desc = 'Initialise storybook on your project';
@@ -30,6 +32,13 @@ export const handler = async (_argv: Arguments): Promise<void> => {
     );
     const token = getToken(_argv);
     await authenticate(token);
+    loader.newStage('Install storybook');
+    const sampleStoriesPath = path.join('src', 'stories');
+    const hadStorybookFolderBefore = fs.existsSync(sampleStoriesPath);
+    execSync('npx storybook@latest init -y', {stdio: 'inherit'});
+    if(!hadStorybookFolderBefore && fs.existsSync(sampleStoriesPath)){
+      fs.rmSync(sampleStoriesPath, { recursive: true, force: true });
+    }
     loader.newStage('Fetching components');
     const files = getJSFiles(_argv.componentsDir as string);
     const componentsWithoutStorybook = files.filter(f => !f.includes(".test.") && !files.includes(`${f.split(".")[0]}.stories.js`));
@@ -45,6 +54,7 @@ export const handler = async (_argv: Arguments): Promise<void> => {
         console.log(`Skipped ${componentFile}. Couldn't generate story config`);
       }
     }
+    console.log("");
     loader.stop();
     log.green('  - Done');
     transaction.status = 'ok';
