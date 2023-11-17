83c6cfddeb1e17e66cf85bb9a663e380070591d6
Moez <mrbouaggadmoez@gmail.com>
2023-11-17 20:56:14 +0100

    add storybook-components

---
+++ packages/storybook-components/src/components/ArgsTable/control/index.ts
@@ -0,0 +1,8 @@
+export { DateControl } from './DateControl';
+export { NumberControl } from './NumberControl';
+export { ObjectControl } from './ObjectControl';
+export { TextControl } from './TextControl';
+export { FilesControl } from './FilesControl';
+export { RangeControl } from './RangeControl';
+export { BooleanControl } from './BooleanControl';
+export { OptionsControl } from './options';
---
+++ packages/storybook-components/src/components/ArgsTable/index.tsx
@@ -0,0 +1,1 @@
+export { ArgsTable } from './ArgsTable';
