83f05e2348ff0506eeccbebdc6f6086a56656eee
Moez <mrbouaggadmoez@gmail.com>
2023-11-17 21:44:11 +0100

    fix: downgrade @storybook/theming

--- packages/storybook-components/src/index.ts
+++ packages/storybook-components/src/index.ts
@@ -3,4 +3,5 @@ import { convert, themes } from '@storybook/theming';
 export const themeDark = convert(themes.dark);
 export const themeLight = convert(themes.light);
 export { ArgsTable } from './components/ArgsTable';
+export { ThemeProvider } from '@storybook/theming';
 export * from './types';
